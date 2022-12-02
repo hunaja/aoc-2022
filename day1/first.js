85dd66ab689f413b4348ed70322186287bb2363a
elias <79339968+awwry@users.noreply.github.com>
2022-12-02 02:31:32 +0200

    Day 1

---
+++ day1/first.js
@@ -0,0 +1,18 @@
+import fs from "fs/promises";
+
+const inputBuffer = await fs.readFile("both.input")
+const input = inputBuffer.toString();
+
+const result = input.split('\n')
+    .reduce((acc, valStr) => {
+        if (!valStr.trim()) {
+            acc.push(0);
+            return acc;
+        }
+
+        acc[acc.length - 1] += Number(valStr);
+        return acc;
+    }, [0])
+    .sort((a, b) => b - a)[0];
+
+console.log(result);
---
+++ day1/second.js
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+
+const inputBuffer = await fs.readFile("both.input")
+const input = inputBuffer.toString();
+
+const result = input.split('\n')
+    .reduce((acc, valStr) => {
+        if (!valStr.trim()) {
+            acc.push(0);
+            return acc;
+        }
+
+        acc[acc.length - 1] += Number(valStr);
+        return acc;
+    }, [0])
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, val) => acc + val, 0);
+
+console.log(result);
