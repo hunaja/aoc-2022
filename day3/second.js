7998300f616d6a02018cfaed5e3d8d4d7a8521d0
elias <79339968+awwry@users.noreply.github.com>
2022-12-03 14:00:50 +0200

    Day 3

---
+++ day3/second.js
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+
+const inputBuffer = await fs.readFile("both.input")
+const input = inputBuffer.toString();
+
+const CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const result = input.split('\n')
+    .reduce((acc, val, i) => {
+        if (i % 3 === 0) acc.push([]);
+        acc.at(-1).push(val);
+        return acc;
+    }, [])
+    .map(([first, second, third]) => {
+        const char = [...first].find((c) => second.includes(c) && third.includes(c));
+        return CHARS.indexOf(char) + 1;
+    })
+    .reduce((acc, val) => acc + val, 0);
+
+console.log(result);
